94e076c9ef5ef8b12f1622017fa085e2b9c90b42
teawon <78795820+teawon@users.noreply.github.com>
2023-05-05 01:44:28 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 40.12 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,18 @@
+function solution(arr)
+{
+    
+    if (arr.length === 0) {
+        return [];
+    }
+    
+    var answer = [];
+    answer.push(arr[0]);
+    
+    for(let i=1; i<arr.length; i++){
+        if(arr[i-1] !== arr[i]){
+            answer.push(arr[i]);
+        }
+    }
+   
+    return answer;
+}
