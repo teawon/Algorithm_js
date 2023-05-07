9d65c8083e4990133d4a34639bb5a9c2bc560a5b
teawon <78795820+teawon@users.noreply.github.com>
2023-05-08 08:54:37 +0900

    [level 2] Title: 올바른 괄호, Time: 2.73 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,24 @@
+function solution(s) {
+
+    var count = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "(") {
+
+            count++;
+
+        } else {
+            count--;
+        }
+
+        if (count < 0) {
+            return false;
+        }
+    }
+
+    if (count !== 0) {
+        return false;
+    }
+    
+    return true;
+}
