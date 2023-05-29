a5cafecd0d67ff0565122e8fc277b0f2b87b2cd5
teawon <78795820+teawon@users.noreply.github.com>
2023-05-30 00:08:26 +0900

    [level 1] Title: 최소직사각형, Time: 9.56 ms, Memory: 42 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,20 +1,25 @@
+/*
+   공부
+let obj = { name: 'John', age: 30 };
+let { name, age } = obj;
+
+let array = [1, 2, 3];
+let [a, b, c] = array;
+
+위 방식과 같이 비구조화 할당을 사용하면 코드를 더 간결하게 표현할 수 있다
+*/
 function solution(sizes) {
 
     var maxWidth = 0
     var maxHeight = 0
+    
     for(let i=0; i<sizes.length; i++){
-        width_1 = sizes[i][0];
-        width_2 = sizes[i][1];
+        let [size1, size2] = sizes[i].sort((a,b)=> b-a);
         
-        if(width_1 < width_2){
-            maxWidth = Math.max(maxWidth,width_2);
-            maxHeight =  Math.max(maxHeight, width_1);
-        }
-        else{
-            maxWidth = Math.max(maxWidth,width_1);
-            maxHeight =  Math.max(maxHeight, width_2);
-        }
+        maxWidth = Math.max(maxWidth,  size1);
+        maxHeight = Math.max(maxHeight,  size2);
     }
     
     return maxWidth*maxHeight;
 }
+
