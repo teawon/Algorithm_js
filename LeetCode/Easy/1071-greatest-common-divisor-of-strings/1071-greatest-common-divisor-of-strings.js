8b71007a583979262c282b00204e4a921ca4342a
teawon <78795820+teawon@users.noreply.github.com>
2023-11-29 03:25:09 +0900

    [Easy] 1071. Greatest Common Divisor of Strings, Time(%): 24.96%, Memory(%): 25.66% - LeetHub

---
+++ LeetCode/Easy/1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function(str1, str2) {
+    // 반복문 돌면서, 확인하기
+    let answer = "";
+
+    for(let i=0; i<str1.length; i++){
+        const divWord = str1.slice(0,i+1); 
+        
+        if(str1.length % divWord.length !== 0) continue;
+        if(str2.length % divWord.length !== 0) continue;
+
+        const str1Len = str1.length / divWord.length;
+        const str2Len = str2.length / divWord.length;
+        
+        if(str1 !== divWord.repeat(str1Len)) continue;
+        if(str2 !== divWord.repeat(str2Len)) continue;
+
+        answer = divWord;
+
+    }
+    return answer;
+};
