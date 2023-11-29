eb3f8c310fed16c023b7e0a410d4f1b27ecdb1ba
teawon <78795820+teawon@users.noreply.github.com>
2023-11-29 14:57:12 +0900

    [Easy] 1071. Greatest Common Divisor of Strings, Time(%): 73.32%, Memory(%): 78.95% - LeetHub

--- LeetCode/Easy/1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
+++ LeetCode/Easy/1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
@@ -3,9 +3,30 @@
  * @param {string} str2
  * @return {string}
  */
+
+/*
+    최대 공통 접두사 길이는 결국 두 문자열 길이의 최대 공약수이다.
+    두 수의 최대 공약수 - 유클리드 호제법 사용하기
+*/
 var gcdOfStrings = function(str1, str2) {
-    // 반복문 돌면서, 확인하기
-    let answer = "";
+
+    function gcd(a , b){
+        return b === 0 ? a : gcd(b, a % b);
+    }
+
+    if(str1 + str2 !== str2 + str1){
+        return ""
+    }
+
+    // 두 문자열의 최대 공약수 계산
+    const lenGCD = gcd(str1.length, str2.length);
+   
+    return str1.slice(0, lenGCD);
+};
+
+/*
+시도했던 것 1) . 반복문을 통해 길이를 계산하는 방법 -> 비효율적
+      let answer = "";
 
     for(let i=0; i<str1.length; i++){
         const divWord = str1.slice(0,i+1); 
@@ -23,4 +44,4 @@ var gcdOfStrings = function(str1, str2) {
 
     }
     return answer;
-};
+*/
