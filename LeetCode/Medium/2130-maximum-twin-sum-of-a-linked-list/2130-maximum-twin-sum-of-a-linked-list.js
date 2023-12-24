82ed56e26a158bd131d5498875377042b84ab93f
teawon <78795820+teawon@users.noreply.github.com>
2023-12-25 00:09:54 +0900

    [Medium] 2130. Maximum Twin Sum of a Linked List, Time(%): 96.07%, Memory(%): 95.71% - LeetHub

--- LeetCode/Medium/2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.js
+++ LeetCode/Medium/2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.js
@@ -10,17 +10,31 @@
  * @return {number}
  */
 var pairSum = function(head) {
-    let curNode = head;
-    let value = [];
-    while(curNode){
-        value.push(curNode.val);
-        curNode = curNode.next;
+    let slow = head; 
+    let fast = head;
+    let reverseFromMid = null;
+    
+    // 중간 지점 찾기
+    while (fast && fast.next) {
+        fast = fast.next.next;
+
+        let nextSlow = slow.next;   // 다음 노드값을 변수에 저장
+        slow.next = reverseFromMid; // 현재값을 기준으로 이전값을 가리키도록 역순으로 저장한다
+        reverseFromMid = slow; // 이전값의 최신값을 현잭값으로 갱신
+
+        slow = nextSlow; // 저장해둔 다음 노드값으로 업데이트
     }
 
+    let leftList = reverseFromMid;
+    let rightList = slow;
+
     let max = 0;
-    for(let i=0; i<value.length / 2; i++){
-        max = Math.max(max, value[i]+value[value.length-i-1]);
+
+    while(leftList && rightList){
+      max = Math.max(max , leftList.val+rightList.val);
+      leftList = leftList.next;
+      rightList = rightList.next;
     }
 
     return max;
-};
+}
